c0cff9bb7058196219b51941c4cc163c0ae8d405
vvspl <vladpolt@gmail.com>
2021-07-25 19:58:54 +0300

    task

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import { now } from 'moment';
+import React from 'react';
 
-import { days } from "../../utils/dateUtils.js";
+import { days } from '../../utils/dateUtils.js';
 
 const Navigation = ({ weekDates }) => {
+  let isCurrentDateOnPage = weekDates.some(
+    dayDate =>
+      dayDate.getDate() === new Date().getDate() &&
+      dayDate.getMonth() === new Date().getMonth() &&
+      dayDate.getYear() === new Date().getYear(),
+  );
+
+  let clsNam = '';
+
   return (
     <header className="calendar__header">
-      {weekDates.map((dayDate) => (
-        <div className="calendar__day-label day-label">
-          <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
+      {weekDates.map(dayDate => (
+        <div className="calendar__day-label day-label" >
+             {
+          dayDate.getDate() === new Date().getDate() && isCurrentDateOnPage
+            ? (clsNam = 'day-label__day-name-now')
+            : (clsNam = 'day-label__day-name')
+          }
+          <span className={clsNam}>{days[dayDate.getDay()]}</span>
           <span className="day-label__day-number">{dayDate.getDate()}</span>
         </div>
       ))}
