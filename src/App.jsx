abc4b1dcefa47382813e92972830edda571bd01b
vvspl <vladpolt@gmail.com>
2021-07-25 12:26:36 +0300

    Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import React, { Component } from "react";
-import Header from "./components/header/Header.jsx";
-import Calendar from "./components/calendar/Calendar.jsx";
-
-import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
-
-import "./common.scss";
+import React, { Component } from 'react';
+import Header from './components/header/Header.jsx';
+import Calendar from './components/calendar/Calendar.jsx';
+import { getWeekStartDate, generateWeekRange, months } from '../src/utils/dateUtils.js';
+import './common.scss';
 
 class App extends Component {
   state = {
+    weekDifference: 0,
     weekStartDate: new Date(),
   };
 
+  nextWeek = () => {
+    this.setState({
+      weekDifference: this.state.weekDifference - 1,
+    });
+  };
+
+  prevWeek = () => {
+    this.setState({
+      weekDifference: this.state.weekDifference + 1,
+    });
+  };
+
+  backToCurrentDate = () => {
+    this.setState({
+      weekDifference: 0,
+    });
+  };
+
   render() {
-    const { weekStartDate } = this.state;
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+    const changedStartDate = new Date(
+      this.state.weekStartDate - this.state.weekDifference * 7 * 24 * 60 * 60 * 1000,
+    );
+
+    const currentMonthMonday = months[getWeekStartDate(changedStartDate).getMonth()];
+    const sundayDate = new Date(
+      getWeekStartDate(changedStartDate).setDate(getWeekStartDate(changedStartDate).getDate() + 6),
+    );
+    const currentMonthSunday = months[sundayDate.getMonth()];
+    let currentMonth = currentMonthMonday;  // Выводим в Хедер название месяца
+    if (currentMonthMonday !== currentMonthSunday) {
+      currentMonth = currentMonthMonday + '-' + currentMonthSunday;
+    }
+    const weekDates = generateWeekRange(getWeekStartDate(changedStartDate));
 
     return (
       <>
-        <Header />
+        <Header
+          nextWeek={this.nextWeek}
+          prevWeek={this.prevWeek}
+          backToCurrentDate={this.backToCurrentDate}
+          currentMonth={currentMonth}
+        />
         <Calendar weekDates={weekDates} />
       </>
     );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,21 +4,21 @@ import { months } from "../../utils/dateUtils.js";
 
 import "./header.scss";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className="header">
       <button className="button create-event-btn">
         <i className="fas fa-plus create-event-btn__icon"></i>Create
       </button>
       <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="navigation__today-btn button" onClick={()=>props.backToCurrentDate()}>Today</button>
+        <button className="icon-button navigation__nav-icon" onClick={()=>props.prevWeek()}>
           <i className="fas fa-chevron-left"></i>
         </button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="icon-button navigation__nav-icon" onClick={()=>props.nextWeek()}>
           <i className="fas fa-chevron-right"></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <span className="navigation__displayed-month">{props.currentMonth}</span>
       </div>
     </header>
   );
